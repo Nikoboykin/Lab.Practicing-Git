060ae8e87a8b8d9b1c21b3a41f15491d47663932
Niko <nikoboykin@gmail.com>
2022-07-07 14:18:19 -0400

    classwork

--- Project_03/Tic-Tac-Toe/index.js
+++ Project_03/Tic-Tac-Toe/index.js
@@ -1,7 +1,19 @@
 const cells = document.querySelectorAll(".cell");
 const turnStatus = document.querySelector("#turnStatus");
-const currentPlayerName = document.querySelector("#player1");
 const restartBttn = document.querySelector("#restartBttn");
+const player1 = document.getElementById("player1");
+const player2 = document.getElementById("player2"); 
+const players = [player1, player2];
+
+function getPlayerName(){
+}
+player1.addEventListener = ("change", (event) => {
+    // let player1Name = player1;
+    player1.innerText = event.target.value;
+    console.log(player1.innerText);
+
+})
+
 const winConditions = [
     
     [0, 1, 2],
@@ -14,12 +26,16 @@ const winConditions = [
     [2, 4, 6] 
 ];
 let options = ["","","","","","","","",""];
-let currentPlayer = "X" ;
+let startPlayer = players[getRandomInt(2)];
+let currentPlayer = "X";
 let running = false;
+console.dir(startPlayer);
+
 
 initializeGame();
 
 function initializeGame(){
+    
 
     cells.forEach(cell => cell.addEventListener("click",cellClicked));
 
@@ -27,7 +43,12 @@ function initializeGame(){
 
     turnStatus.textContent = (`${currentPlayer} Choose Your Space!`);
     running = true;
+    getPlayerName();
 }
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
 function cellClicked(){
     let cellIdx = this.getAttribute("cellIdx");
 
